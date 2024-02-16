af0fb6402c789e0bf51e45c580a50d79d14a992d
Lau <laurentiucozma12@gmail.com>
2024-02-16 19:48:05 +0200

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,21 +1,27 @@
-import Header from './Header.jsx'
-import Food from './Food.jsx'
-import Card from './Card.jsx'
-import Button from './Button.jsx'
-import Footer from './Footer.jsx'
+// import Header from './Header.jsx'
+// import Food from './Food.jsx'
+// import Card from './Card.jsx'
+import Student from './Student.jsx'
+// import Button from './Button.jsx'
+// import Footer from './Footer.jsx'
 
 function App() {
   
   return(
     <>
-      <Header/>
+      {/* <Header/>
       <Food/>
       <Card/>
       <Card/>
       <Card/>
       <Card/>
       <Button/>
-      <Footer/>
+      <Footer/> */}
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+
     </>
   );
 }
